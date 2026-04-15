a59c0162fb94284881ac02ce317e8ee1f6589a8a
S263100 <s263100@uos.ac.uk>
2026-04-15 17:55:38 +0100

    Completed registration route and user model

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,13 +1,71 @@
 import express from "express";
+import User from "../models/Users.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+const generateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
 
 router.get("/login", async (req, res) => {
     res.send("Login successful");
 });
 
-router.get("/register", async (req, res) => {
-    res.send("Registration successful");
+router.post("/register", async (req, res) => {
+    try {
+       const { email, username, password } = req.body;
+
+       // If User doesn't fill in every field, return a message to fill in all fields
+       if (!email || !username || !password) {
+        return res.status(400).json({ message: "Please fill in all fields" });
+       }
+
+       // If password is shorter than 6 characters, return a message instructing a longer password
+       if (password.length < 6) {
+        return res.status(400).json({ message: "Password must be at least 6 characters" });
+       }
+
+       // If username is shorter than 3 characters, return a message instructing a longer username
+       if (username.length < 3) {
+        return res.status(400).json({ message: "Username must be at least 3 characters" });
+       }
+
+       // Check if email is already in use
+       const existingUser = await User.findOne({ email });
+       if (existingUser) {
+        return res.status(400).json({ message: "Email is already in use" });
+       }
+
+       // Check if username is already in use
+       const existingUsername = await User.findOne({ username });
+       if (existingUsername) {
+        return res.status(400).json({ message: "Username already in use" });
+       }
+
+       //Create new user if all checks are passed
+       const user = new User({
+        email,
+        username,
+        password
+       })
+
+       await user.save();
+
+       const token = generateToken(user._id);
+
+       res.status(201).json({
+        token,
+        user:{
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    });   
+    } catch (error) {
+        console.log("Error in register route:", error);
+        res.status(500).json({ message: "Internal Server error" });
+    }   
 });
 
 export default router;
