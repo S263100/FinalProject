b96ac539b4d9029a684dcadc413d5087abc4302b
S263100 <s263100@uos.ac.uk>
2026-04-15 15:41:31 +0100

    Improved backend structure

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
-import { connectDB } from "./lib/db.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
