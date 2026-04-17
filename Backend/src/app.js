9e22f4b738d9fcadf0009abffa4356b33d69f2e9
S263100 <s263100@uos.ac.uk>
2026-04-17 01:24:52 +0100

    Added Backend Functionality for the Login and Register Pages

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
--- Frontend/gym-tracker/src/pages/LoginPage.jsx
+++ Frontend/gym-tracker/src/pages/LoginPage.jsx
@@ -1,12 +1,56 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
-  return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+        console.log("Please fill all fields");
+        return;
+    }
+
+    try {
+        const res = await fetch("http://localhost:5001/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            console.log("Login failed:", data.message);
+            return;
+        }
+
+        console.log("Login Success:", data);
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+
+        navigate("/");
+
+    } catch (error) {
+        console.log("Error during login:", error);
+    }
+    };
+
+    
+    return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <form className="bg-white p-8 rounded shadow-md w-full max-w-sm space-y-4">
+      <form onSubmit={handleSubmit} className="bg-white p-8 rounded shadow-md w-full max-w-sm space-y-4">
       <h1 className="text-3xl font-bold flex items-center justify-center">Login</h1>
 
-      <input type="text" placeholder="Username or Email" className="w-full p-2 mb-4 border border-gray-300 rounded" />
+      <input type="text" placeholder="Email" className="w-full p-2 mb-4 border border-gray-300 rounded" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-      <input type="password" placeholder="Password" className="w-full p-2 mb-4 border border-gray-300 rounded" />
+      <input type="password" placeholder="Password" className="w-full p-2 mb-4 border border-gray-300 rounded" value={password} onChange={(e) => setPassword(e.target.value)} />
 
       <button type="submit" className="w-full bg-primary text-gray-800 p-2 rounded hover:bg-primary-dark transition-colors duration-200">Login</button>
       </form>
