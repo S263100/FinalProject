c0210df45e55430a1e64274588a4ac89b61dee9b
S263100 <s263100@uos.ac.uk>
2026-05-18 20:46:58 +0100

    Created initial dahboard graphs (Will be changed)

--- Backend/src/controllers/WorkoutController.js
+++ Backend/src/controllers/WorkoutController.js
@@ -21,19 +21,27 @@ export const getWorkoutStats = async (req, res) => {
     
     const userId = new mongoose.Types.ObjectId(req.user.id);
 
-    //Exercise rep amount for each exercise aggregation
+    //Exercise rep amount for each exercise overall
     const exerciseRepTotals = await WorkoutResults.aggregate([
         { $match: { userId }},
         { $unwind: "$exerciseResults"},
         { $group: {_id: "$exerciseResults.exerciseId", totalReps: {$sum : "$exerciseResults.totalReps"}}},
         { $lookup: {from: "exercises", localField: "_id", foreignField: "_id", as: "exerciseInfo"}},
-        { $project: {_id: 0, exerciseId: "$_id", exerciseName: {$arrayElemAt: ["$exerciseInfo.name", 0]}, totalReps: 1}}])
+        { $project: {_id: 0, exerciseId: "$_id", exerciseName: {$arrayElemAt: ["$exerciseInfo.name", 0]}, totalReps: 1}}]);
 
+    //Daily exercise rep amount
+    const exerciseRepTotalsDaily = await WorkoutResults.aggregate([
+        { $match: { userId }},
+        { $unwind: "$exerciseResults"},
+        { $group: {_id: { date: { $dateToString: {format: "%Y-%m-%d", date: "$startTime"}}, exerciseId: "$exerciseResults.exerciseId"}, totalReps: {$sum : "$exerciseResults.totalReps"}}},
+        { $lookup: {from: "exercises", localField: "_id.exerciseId", foreignField: "_id", as: "exerciseInfo"}},
+        { $project: {_id: 0, date: "$_id.date", exerciseId: "$_id", exerciseName: {$arrayElemAt: ["$exerciseInfo.name", 0]}, totalReps: 1}},
+        { $sort: { date: 1 }}]);
+        
     //Total workout time aggregatiion
     const workoutTimeTotal = await WorkoutResults.aggregate([
         { $match: { userId }},
-        { $group: {_id: null, totalWorkoutTime: { $sum: "$durationSeconds" }}}
-    ])
+        { $group: {_id: null, totalWorkoutTime: { $sum: "$durationSeconds" }}}]);
      
     const totalWorkoutTime = workoutTimeTotal[0]?.totalWorkoutTime || 0 
 
@@ -43,5 +51,5 @@ export const getWorkoutStats = async (req, res) => {
     //Average time spent on a full workoutt
     const averageWorkoutTime = totalWorkouts > 0 ? totalWorkoutTime / totalWorkouts : 0;
 
-    return res.json({ exerciseRepTotals, totalWorkoutTime, averageWorkoutTime })
+    return res.json({ exerciseRepTotals, exerciseRepTotalsDaily, totalWorkoutTime, averageWorkoutTime })
     };
--- Frontend/gym-tracker/src/pages/DashboardPage.jsx
+++ Frontend/gym-tracker/src/pages/DashboardPage.jsx
@@ -2,15 +2,25 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, PieChart, Pie, Cell, Legend} from "recharts";
 
 const DashboardPage = () => {
     const [stats, setStats] = useState(null);
 
     const navigate = useNavigate();
 
+    const repTotal = (stats?.exerciseRepTotals || [])
+
+    const totalOverallReps = repTotal.reduce((sum, ex) => sum + ex.totalReps, 0);
+
     const calculateMinutes = Math.floor((stats?.totalWorkoutTime || 0) / 60);
 
     const averageWorkoutTime = Math.floor((stats?.averageWorkoutTime || 0) / 60);
+
+    const pieChartData = repTotal.map(ex => ({
+      name: ex.exerciseName,
+      value: totalOverallReps ? Math.round((ex.totalReps / totalOverallReps) * 100) : 0
+    }));
 
     useEffect(() => {
         const token = localStorage.getItem("token");
@@ -44,7 +54,7 @@ const DashboardPage = () => {
       <div className="w-full max-w-7xl bg-white p-8 rounded-2xl shadow-lg">
       <h1 className="text-4xl font-bold mb-6 text-center">Dashboard</h1>
 
-      <div className="grid grid-cols-3 gap-4 mt-6">
+      <div className="grid grid-cols-2 gap-4 mt-6">
         <div className="bg-white p-4 rounded shadow">
           <h2 className="text-xl font-bold mb-2 underline">Total Reps Per Exercise:</h2>
           {stats?.exerciseRepTotals?.map(exercise => (
@@ -64,6 +74,34 @@ const DashboardPage = () => {
           <h2 className="text-xl font-bold mb-2 underline">Average Workout Duration</h2>
           <p className="text-xl font-bold mb-2">{averageWorkoutTime} mins</p>
         </div>
+
+        <div className="bg-white p-4 rounded shadow">
+          <BarChart width={350} height={300} data={stats?.exerciseRepTotals}>
+            <XAxis dataKey="exerciseName"/>
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="totalReps" fill="#8884d8"/>
+          </BarChart>
+        </div>
+
+        <div className="bg-white p-4 rounded shadow">
+          <BarChart width={600} height={300} data={stats?.exerciseRepTotalsDaily}>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="date"/>
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="totalReps" name="reps" fill="#8884d8"/>
+          </BarChart>
+        </div>
+
+        <div className="bg-white p-4 rounded shadow">
+          <PieChart width={400} height={300}>
+            <Pie data={pieChartData} dataKey="value" nameKey="name" outerRadius={100} label/>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </div>
+
       </div>
       </div>
     </div>
